74209dffc2187fc41670a8558e6706f05cd81af6
LAVAKUMAR-P <lavakumar16000@gmail.com>
2021-09-04 12:34:49 +0530

    db added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://lavakumar:lavakumar@123@cluster0.mu3ky.mongodb.net?retryWrites=true&w=majority";
 const PORT=process.env.PORT || 3001
 
 app.use(
