36e9b8bb045fd8386ec725be86f877f64ed7fc8d
LAVAKUMAR-P <lavakumar16000@gmail.com>
2021-09-09 20:50:46 +0530

    axios removed

--- index.js
+++ index.js
@@ -3,14 +3,88 @@ const app = express();
 const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-const url = "mongodb+srv://lava:lava123@cluster0.py3np.mongodb.net?retryWrites=true&w=majority";
+const bcryptjs = require('bcrypt');
+const url = "mongodb://localhost:27017";
+//"mongodb+srv://lava:lava123@cluster0.py3np.mongodb.net?retryWrites=true&w=majority";
 const PORT=process.env.PORT || 3001
+
 
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use(express.json());
+app.post("/register", async function (req, res) {
+  try {
+      // Connect the Database
+      let client = await mongoClient.connect(url)
+
+      // Select the DB
+      let db = client.db("todo_app");
+
+      // Hash the password
+      console.log(req.body.password);
+      let salt = bcryptjs.genSaltSync(10);
+      let hash = bcryptjs.hashSync(req.body.password,salt);
+      req.body.password = hash;
+     // console.log(hash);
+
+      // Select the Collection and perform the action
+      let data = await db.collection("task").insertOne(req.body)
+
+      // Close the Connection
+      await client.close();
+
+      res.json({
+          message: "User Registered",
+          id: data._id
+      })
+  } catch (error) {
+        console.log(error);
+        res.json({
+          message: "Registeration failed"
+      })
+  }
+})
+
+//login
+app.post("/login", async function (req, res) {
+  try {
+      // Connect the Database
+      let client = await mongoClient.connect(url)
+
+      // Select the DB
+      let db = client.db("todo_app");
+
+      // Find the user with email_id
+      let user = await db.collection("task").findOne({ username: req.body.username });
+
+      if (user) {
+          // Hash the incoming password
+          // Compare that password with user's password
+          console.log(req.body)
+          console.log(user.password)
+          let matchPassword = bcryptjs.compareSync(req.body.password, user.password)
+         if(matchPassword){
+          res.json({
+            message: "true"
+        })
+         }
+      } else {
+          res.status(404).json({
+              message: "Username/Password is incorrect"
+          })
+      }
+
+
+
+
+
+  } catch (error) {
+      console.log(error)
+  }
+})
 
 app.get("/list-all-to-do", async function (req, res) {
   try {
@@ -34,7 +108,7 @@ app.get("/list-all-to-do", async function (req, res) {
     });
   }
 });
-app.use(express.json());
+
 
 app.post("/create-task", async function (req, res) {
   try {
